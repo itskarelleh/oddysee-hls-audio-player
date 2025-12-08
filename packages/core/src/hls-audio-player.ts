731ae12def985e0fe0907bc181e09f304ac74e27
itskarelleh <karellehofler@gmail.com>
2025-12-08 17:52:13 -0500

    moved HLSAudioPlayerInterface to hls-audio-player.ts, deleted extra file

--- packages/core/src/hls-audio-player.ts
+++ packages/core/src/hls-audio-player.ts
@@ -9,7 +9,34 @@ import {
 	PlayerEventMap,
 	PlayerState,
 } from './types';
-import type { HLSAudioPlayerInterface } from './interface';
+
+export interface HLSAudioPlayerInterface {
+	setSource(url: string, options?: SourceOptions): Promise<HLSAudioPlayer>;
+	getCurrentTrack(): Track | null;
+
+	on<K extends PlayerEvent>(
+		event: K,
+		callback: (data: PlayerEventMap[K]) => void,
+	): void;
+	off<K extends PlayerEvent>(
+		event: K,
+		callback: (data: PlayerEventMap[K]) => void,
+	): void;
+
+	play(): HLSAudioPlayer;
+	playAsync(): Promise<HLSAudioPlayer>;
+	pause(): HLSAudioPlayer;
+	setVolume(volume: number): HLSAudioPlayer;
+
+	getState(): PlayerState;
+	getAudioElement(): HTMLAudioElement;
+
+	loading: boolean;
+	readyState: number;
+	error: PlayerError | null;
+
+	destroy(): void;
+}
 
 type EventCallback<K extends PlayerEvent = PlayerEvent> = (
 	data: PlayerEventMap[K],
