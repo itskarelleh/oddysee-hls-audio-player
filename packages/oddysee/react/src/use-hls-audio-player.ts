5581893aa4e824728c3891b1c995a887d2111395
itskarelleh <karellehofler@gmail.com>
2026-02-15 08:34:18 -0500

    updated for 0.2.0

--- packages/oddysee/react/src/use-hls-audio-player.ts
+++ packages/oddysee/react/src/use-hls-audio-player.ts
@@ -20,7 +20,7 @@ import {
   type QualityLevel,
   type PlayerState
 } 
-from '../../typescript/src'
+from 'oddysee-typescript'
 
 // Local mirror of the core's PlayerEventMap so we don't depend on it being exported
 export type PlayerEventMap = {
